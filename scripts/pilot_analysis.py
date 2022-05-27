68719ad758460bfa84430d65d2f393f08efb1c17
Levan Bokeria <levan.bokeria@gmail.com>
2022-05-27 12:15:21 +0100

    added a qc check on easiest trials

--- scripts/pilot_analysis.py
+++ scripts/pilot_analysis.py
@@ -64,7 +64,7 @@ ind_exp = []
 #                                    header = None)
 
 # if it doesn't just start the loop
-for iF in file_list[90:100]:
+for iF in file_list:
     print(iF)
     
     f = open('./data/pilots/gui_downloads/' + iF,'r')    
